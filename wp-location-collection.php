bf3871f8cb93f8b45a454858db1862f02f423d01
Jess Green <2304490+jesgs@users.noreply.github.com>
2025-10-30 13:29:17 -0500

    Plugin scaffold/initial commit

---
+++ wp-location-collection.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wp Location Collection
+ * Plugin URI:
+ * Description:     Location data collection plugin for Gravity Forms
+ * Author:          NAMI.org
+ * Author URI:
+ * Text Domain:     wp-location-collection
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Location_Collection
+ */
+
+// Your code starts here.

bcfa2c9c887b87b802e109b546edf77b20e2a3ea
Jess Green <2304490+jesgs@users.noreply.github.com>
2025-10-30 14:35:54 -0500

    Stub methods for activating/deactivating plugins

--- wp-location-collection.php
+++ wp-location-collection.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:     Wp Location Collection
+ * Plugin Name:     Location Data Collection
  * Plugin URI:
  * Description:     Location data collection plugin for Gravity Forms
  * Author:          NAMI.org
@@ -9,7 +9,32 @@
  * Domain Path:     /languages
  * Version:         0.1.0
  *
- * @package         Wp_Location_Collection
+ * @package         Nami_Location_Collection
  */
 
 // Your code starts here.
+
+register_activation_hook(
+	__FILE__,
+	['Nami_Location_Collection', 'activate']
+);
+
+register_deactivation_hook(
+	__FILE__,
+	[ 'Nami_Location_Collection', 'deactivate' ]
+);
+
+register_uninstall_hook(
+	__FILE__,
+	['Nami_Location_Collection', 'uninstall']
+);
+class Nami_Location_Collection {
+
+	public static function activate() {
+		// check for GravityForms and if not present, deactivate?
+	}
+
+	public static function deactivate() {}
+
+	public static function uninstall() {}
+}

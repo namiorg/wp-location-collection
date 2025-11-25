8cab46987303c1c51ab1a9d1bc0d620b589c7c56
Jess Green <2304490+jesgs@users.noreply.github.com>
2025-11-25 12:36:18 -0600

    Advance minor version

--- wp-location-collection.php
+++ wp-location-collection.php
@@ -7,7 +7,7 @@
  * Author URI:
  * Text Domain:     nami-location-collection
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.1.1
  *
  * @package         Nami_Location_Collection
  */
@@ -39,7 +39,7 @@ register_uninstall_hook(
  */
 class Nami_Location_Collection {
 
-	const string VERSION = '0.1.0';
+	const string VERSION = '0.1.1';
 
 	/**
 	 * Plugin activation hook

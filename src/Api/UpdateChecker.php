1f35abc5faaa584cef532363cfc021a962722ad1
Jess Green <2304490+jesgs@users.noreply.github.com>
2025-12-19 15:39:23 -0600

    Add stub methods

--- src/Api/UpdateChecker.php
+++ src/Api/UpdateChecker.php
@@ -4,7 +4,8 @@ namespace Nami\LocationData\Api;
 
 class UpdateChecker
 {
-	const string MANIFEST_JSON_URL = 'https://api.github.com/repos/namiorg/wp-location-collection/blob/main/manifest.json';
+	protected const string RELEASE_ENDPOINT = 'https://api.github.com/repos/namiorg/wp-location-collection/releases';
+	protected const int|float CACHE_TIME = 12 * HOUR_IN_SECONDS;
 
 	protected static ?self $instance = null;
 
@@ -19,6 +20,13 @@ class UpdateChecker
 
 	public function init(): void
 	{
-		// Implementation for update checking goes here
+		add_filter( 'plugins_api', [ $this, 'get_plugin_info' ], 20, 3 );
+		add_filter( 'site_transient_update_plugins', [ $this, 'check_for_update' ] );
+		add_action( 'upgrader_process_complete', [ $this, 'purge' ], 10, 2 );
 	}
+
+	public function get_plugin_info( $false, $action, $args ) {}
+	public function check_for_update( $transient, $plugin_data, $wp_filesystem ) {}
+
+	public function purge( $transient, $plugin_data, $network_wide ) {}
 }
